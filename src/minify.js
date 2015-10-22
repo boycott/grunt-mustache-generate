cc0b39801bf124b03011134206d674d58dc525e8
geoff <Geoff.Kaile@barclayscapital.com>
2015-10-22 13:09:11 +0100

    Mustache tags also need removing of whitespace

--- src/minify.js
+++ src/minify.js
@@ -1,15 +1,40 @@
 'use strict';
 /**
- * Renders HTML without whitespace.
+ * Returns HTML without whitespace around html tags.
+ *
+ * @param html {String} HTML to parse.
+ *
+ * @return html {String} Minified HTML.
+*/
+function pageHTML (html) {
+  html = html.replace(/[\f\t\n\r\v]+/g, ''); // Remove all non-space whitespace characters.
+  html = html.replace(/\s+/g, ' '); // Only one space is ever needed in html.
+  html = html.replace(/\s+</g, '<'); // Remove empty spaces before opening tags.
+  html = html.replace(/>\s+/g, '>'); // Remove empty spaces after closing tags.
+  html = html.replace(/>\s+</g, '><'); // Remove empty spaces between a closing html tag and it's content.
+  return html;
+}
+/**
+ * Renders HTML without whitespace around mustache tags.
+ *
+ * @param html {String} HTML to parse.
+ *
+ * @return html {String} Minified HTML.
+*/
+function mustacheHTML (html) {
+  html = html.replace(/}{2}\s+\{{2}/g, '}}{{');
+  return html;
+}
+
+/**
+ * Returns HTML without any significant whitespace.
  *
  * @param html {String} HTML to parse.
  *
  * @return html {String} Minified HTML.
 */
 module.exports = function (html) {
-  html = html.replace(/[\f\t\n\r\v]+/g, ''); // Remove all non-space whitespace characters.
-  html = html.replace(/\s+/g, ' '); // Only one space is ever needed in html.
-  html = html.replace(/\s</g, '<'); // Remove empty spaces before an opening html tag.
-  html = html.replace(/>\s/g, '>'); // Remove empty spaces between a closing html tag and it's content.
+  html = mustacheHTML(html);
+  html = pageHTML(html);
   return html;
 };
