7004528594dbff9d8619d4dccfd36bf916ebd2b8
geoff <Geoff.Kaile@barclayscapital.com>
2015-11-05 14:23:19 +0000

    Loosening the minification as it was breaking white space in the html

--- src/minify.js
+++ src/minify.js
@@ -9,9 +9,6 @@
 function pageHTML (html) {
   html = html.replace(/[\f\t\n\r\v]+/g, ''); // Remove all non-space whitespace characters.
   html = html.replace(/\s+/g, ' '); // Only one space is ever needed in html.
-  html = html.replace(/\s+</g, '<'); // Remove empty spaces before opening tags.
-  html = html.replace(/>\s+/g, '>'); // Remove empty spaces after closing tags.
-  html = html.replace(/>\s+</g, '><'); // Remove empty spaces between a closing html tag and it's content.
   return html;
 }
 /**
