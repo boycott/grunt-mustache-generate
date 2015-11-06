44b219e21b6472ec549aeb2f5a76d231d3b8779c
geoff <Geoff.Kaile@barclayscapital.com>
2015-11-06 11:39:50 +0000

    Whitespace handling

--- src/minify.js
+++ src/minify.js
@@ -8,10 +8,13 @@
 */
 module.exports = function (html) {
   return html
-    .replace(/(\}{2})\s+(\{{2})/g, '$1$2')  // Remove space around mustache tags.
-    .replace(/[\f\n\r\t\v]+/g, '')          // Remove all non-space whitespace characters.
+    .replace(/(\}{2})\s+(\{{2})/g, '$1$2')      // Remove space within mustache tags.
+    .replace(/(>)\s+(\{{2})/g, '$1$2')          // Remove space before mustache tags.
+    .replace(/(\}{2})\s+(<)/g, '$1$2')          // Remove space after mustache tags.
+    .replace(/[\f\n\r\t\v]+/g, '')              // Remove all non-space whitespace characters.
     .replace(/(>?)\s+(<?)/g, function (match, p1, p2) {
-        var rep = p1 + p2;                  // Output html tags.
-        return rep || ' ';                  // If no tags, return a single whitespace character.
+      var begin = (p1 || ' ');                  // Return the close tag or a space.
+      var end = (p2 || ' ');                    // Plus the open tag or a space.
+      return (begin + end).replace(/\s+/, ' '); // But only one space maximum (as html doesn't display more than one.
     })
 };
